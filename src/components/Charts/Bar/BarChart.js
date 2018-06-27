dbf9d4effbb8faa09f8d8548c607d626b463860f
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-06-27 08:57:58 +0200

    chart.js updates

--- src/components/Charts/Bar/BarChart.js
+++ src/components/Charts/Bar/BarChart.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
-const BarChart = (props) => (
-  <Bar data={props.chartData} options={{
-    legend: {
-      display: false
+const dataObject = {
+  "labels": [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12"
+  ],
+  "datasets": [
+    {
+      "data": [],
+      "backgroundColor": [
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)",
+        "rgba(96, 123, 255,0.2)"
+      ],
+      "borderColor": [
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)",
+        "rgba(96, 123, 255,1)"
+      ],
+      "borderWidth": 1
     }
-  }} />
-);
+  ]
+}
+const BarChart = (props) => {
+  dataObject.datasets[0].data = [...props.chartData];
+  return (
+    <Bar data={dataObject} options={{
+      legend: {
+        display: false
+      }
+    }} />
+  );
+}
 
 export default BarChart;
--- src/components/SalesReport/SalesReport.js
+++ src/components/SalesReport/SalesReport.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Grid, Row, Col, DropdownButton, MenuItem } from 'react-bootstrap';
 import BarChart from '../Charts/Bar/BarChart';
+import Aux from '../../hoc/Auxiliary/Auxiliary'
 
 const SalesReport = (props) => (
-  <Grid fluid>
-    <Row>
-      <Col md={6} className='font-white'>
-        <h3><strong>Sales Report</strong></h3>
-      </Col>
-      <Col md={6} className="text-right">
+  <Aux>
+    <div className='SalesReport'>
+      <div style={{ fontWeight: 'bold', fontSize: 'large' }} className='font-white'>
+        Sales Report
+      </div>
+      <div className="text-right">
         <DropdownButton
           onSelect={props.selected}
           bsStyle="default"
@@ -17,14 +18,10 @@ const SalesReport = (props) => (
           <MenuItem eventKey="2018">2018</MenuItem>
           <MenuItem eventKey="2017">2017</MenuItem>
         </DropdownButton>
-      </Col>
-    </Row>
-    <Row>
-      <Col md={12}>
-        <BarChart chartData={props.chartBarData} />
-      </Col>
-    </Row>
-  </Grid>
+      </div>
+    </div>
+    <BarChart chartData={props.chartBarData} />
+  </Aux>
 );
 
 export default SalesReport;
