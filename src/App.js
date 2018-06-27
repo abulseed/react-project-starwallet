ce8bbd635232cc513c20e5119ac703a761ff5697
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-06-27 08:56:41 +0200

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Layout from './hoc/Layout/Layout';
 import HomeGrid from './containers/HomeGrid/HomeGrid';
 import ProjectsGrid from './containers/ProjectsGrid/ProjectsGrid';
@@ -7,8 +9,34 @@ import DataStore from './assets/data/data-store.json';
 class App extends Component {
   state = {
     chartLineData: DataStore.chartDataLine,
-    chartBarData2018: DataStore.chartDataBar['2018'],
-    chartBarData2017: DataStore.chartDataBar['2017'],
+    chartBarData2018: [
+      10,
+      15,
+      20,
+      5,
+      25,
+      30,
+      12,
+      8,
+      26,
+      23,
+      14,
+      3
+    ],
+    chartBarData2017: [
+      2,
+      11,
+      21,
+      50,
+      20,
+      30,
+      42,
+      38,
+      26,
+      3,
+      24,
+      33
+    ],
     currentBarData: DataStore.chartDataBar['2018'],
     projects: DataStore.projects,
     emails: DataStore.emails,
@@ -25,30 +53,43 @@ class App extends Component {
   }
 
   salesYearSelectedHandler = async (key, event) => {
-    console.log(event);
-    let tmp = { ...this.state.currentBarData };
-    if (key == '2018') {
-      tmp = { ...this.state.chartBarData2018 };
-    } else if (key == '2017') {
-      tmp = { ...this.state.chartBarData2017 };
-    }
-    await this.setState({ currentBarData: tmp, selectedYear: key });
-    console.log(this.state)
+    await this.setState({ selectedYear: key });
   }
 
   render() {
+    let barChart = {};
+    if (this.state.selectedYear === '2018') {
+      barChart = this.state.chartBarData2018;
+    }
+
+    if (this.state.selectedYear === '2017') {
+      barChart = this.state.chartBarData2017;
+    }
+
     return (
       <Layout>
-        <HomeGrid
-          emails={this.state.emails}
-          chartLineData={this.state.chartLineData}
-          projects={this.state.projects}
-          selectedYear={this.state.selectedYear}
-          selected={this.salesYearSelectedHandler}
-          clicked={this.emailReadHandler}
-          currentBarData={this.state.currentBarData}
-          date={this.state.date} />
-        {/* <ProjectsGrid projects={this.state.projects} /> */}
+        <Switch>
+          <Route path='/projects' render={
+            props => (
+              <ProjectsGrid {...props} projects={this.state.projects} />
+            )
+          } />
+          <Route path='/' exact
+            render={
+              props => (
+                <HomeGrid
+                  {...props}
+                  emails={this.state.emails}
+                  chartLineData={this.state.chartLineData}
+                  projects={this.state.projects}
+                  selectedYear={this.state.selectedYear}
+                  selected={this.salesYearSelectedHandler}
+                  clicked={this.emailReadHandler}
+                  currentBarData={barChart}
+                  date={this.state.date} />
+              )
+            } />
+        </Switch>
       </Layout>
     );
   }
